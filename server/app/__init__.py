8d201e080ce48f0f781497ea6e3f8e5b806579f8
Igor Borodin <b.igor.work21@gmail.com>
2023-08-05 10:35:01 +0700

    Tables reflection start

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -4,32 +4,22 @@ from sqlalchemy.ext.automap import automap_base
 from loguru import logger
 
 
-
 db = SQLAlchemy()
 app = Flask(__name__)
-
 app.config.from_object('config.DevelopmentConfig')
-db.init_app(app)
+db = SQLAlchemy(app)
 
 
 with app.app_context():
-    eng = db.get_engine()
     Base = automap_base()
-    Base.prepare(eng, reflect=True) 
-    # tables = Base.metadata.tables
+    Base.prepare(db.engine, reflect=True)
     Artist = Base.classes['artist']
-    logger.debug(Artist)
 
 
 @app.route("/")
 def homepage():
-    # logger.debug(dir(Artist))
-    # return ''
-    # logger.debug(Artist.query.all())
-    # result = db.get_or_404(Artist, 2)
-    # logger.debug(result)
-    rest = db.session.query(Artist.query.all())
-    # logger.debug(type(result))
-    # columns = Artist.columns.keys()
-    # d = [{c: v for c, v in zip(columns, row)} for row in result]
-    return rest
+    logger.debug(type(Artist))
+    u = db.session.query(Artist).all()
+    for r in u:
+        logger.debug(r.artist_name)
+    return {'mthd': 'SMTH'}
